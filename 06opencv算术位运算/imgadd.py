2833f66a85418b37e32f4abede6ecb54a3de28af
xxcfun <echo_dyc@163.com>
2022-03-08 10:01:40 +0800

    feat:图像的加减乘除运算

--- 06opencv算术位运算/imgadd.py
+++ 06opencv算术位运算/imgadd.py
@@ -5,11 +5,32 @@ import numpy as np
 
 
 cv2.namedWindow('result', cv2.WINDOW_NORMAL)
+cv2.namedWindow('result1', cv2.WINDOW_NORMAL)
+cv2.namedWindow('result2', cv2.WINDOW_NORMAL)
+cv2.namedWindow('result3', cv2.WINDOW_NORMAL)
+cv2.namedWindow('result4', cv2.WINDOW_NORMAL)
 
 img1 = cv2.imread('1.jpg')
 
 img2 = np.ones((4032, 3024, 3), np.uint8) * 50
 
-result = cv2.add(img1, img2)
-cv2.imshow('result', result)
+# 原画
+cv2.imshow('result', img1)
+
+# 加法
+result1 = cv2.add(img1, img2)
+cv2.imshow('result1', result1)
+
+# 减法
+result2 = cv2.subtract(img1, img2)
+cv2.imshow('result2', result2)
+
+# 乘法
+result3 = cv2.multiply(img1, img2)
+cv2.imshow('result3', result3)
+
+# 除法
+result4 = cv2.divide(img1, img2)
+cv2.imshow('result4', result4)
+
 cv2.waitKey(0)
